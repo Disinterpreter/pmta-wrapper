b7aff1297a64ff817b1b6a854249344230326406
Kenix157 <Kenix157@b2e8e5e1-9cd8-92f6-82b0-49c0d31916f8>
2013-09-23 15:08:15 +0000

    Restored implementation of the needed functions.

--- trunk/[gamemodes]/[amx]/pmta-wrapper/server/syscalls.lua
+++ trunk/[gamemodes]/[amx]/pmta-wrapper/server/syscalls.lua
@@ -191,6 +191,127 @@ local skinReplace = {
 	[299] = 124		-- Claude
 }
 
+function acos(amx, f)
+	return float2cell(math.acos(f))
+end
+
+
+function asin(amx, f)
+	return float2cell(math.asin(f))
+end
+
+
+function atan(amx, f)
+	return float2cell(math.atan(f))
+end
+
+
+function atan2(amx, x, y)
+	return float2cell(math.atan2(y, x))
+end
+
+
+function db_close(amx, db)
+	sqlite3CloseDB(amx.cptr, db)
+end
+
+
+function db_free_result(amx, dbResultID)
+	amx.dbresults[dbResultID] = nil
+end
+
+
+function db_field_name(amx, dbresult, fieldIndex, outbuf, maxlength)
+	local colname = dbresult.columns[fieldIndex+1]
+	if #colname < maxlength then
+		writeMemString(amx, outbuf, colname)
+		return true
+    end
+    return false
+end
+
+
+function db_get_field(amx, dbresult, fieldIndex, outbuf, maxlength)
+	if dbresult[dbresult.row] then
+        local data = dbresult[dbresult.row][fieldIndex+1]
+        if #data < maxlength then
+			writeMemString(amx, outbuf, data)
+			return true
+		end
+	end
+    return false
+end
+
+
+function db_get_field_assoc(amx, dbresult, fieldName, outbuf, maxlength)
+	local fieldIndex = table.find(dbresult.columns, fieldName)
+	return fieldIndex and db_get_field(amx, dbresult, fieldIndex-1, outbuf, maxlength)
+end
+
+
+function db_next_row(amx, dbresult)
+	dbresult.row = dbresult.row + 1
+end
+
+
+function db_num_fields(amx, dbresult)
+	return #dbresult.columns
+end
+
+
+function db_num_rows(amx, dbresult)
+	return #dbresult
+end
+
+
+function db_open(amx, dbName)
+	return sqlite3OpenDB(amx.cptr, dbName)
+end
+
+
+function db_query(amx, db, query)
+	local dbresult = sqlite3Query(amx.cptr, db, query)
+	if type(dbresult) == 'table' then
+		dbresult.row = 1
+		return table.insert(amx.dbresults, dbresult)
+	end
+	return 0
+end
+
+
+function floatstr(amx, str)
+	return float2cell(tonumber(str) or 0)
+end
+
+
+function format(amx, outBuf, outBufSize, fmt, ...)
+	local args = { ... }
+	local i = 0
+        
+	fmt = fmt:gsub('[^%%]%%$', '%%%%'):gsub('%%i', '%%d')
+	for c in fmt:gmatch('%%[%-%d%.]*(%*?%a)') do
+		i = i + 1
+		if c:match('^%*') then
+			c = c:sub(2)
+			table.remove(args, i)
+		end
+		if c == 'd' then
+			args[i] = amx.memDAT[args[i]]
+		elseif c == 'f' then
+			args[i] = cell2float(amx.memDAT[args[i]])
+		elseif c == 's' then
+			args[i] = readMemString(amx, args[i])
+		else
+			i = i - 1
+		end
+    end
+    fmt = fmt:gsub('(%%[%-%d%.]*)%*(%a)', '%1%2')
+    local result = fmt:format(unpack(args))
+    if #result+1 <= outBufSize then
+		writeMemString(amx, outBuf, result)
+	end
+end
+
 g_SAMPSyscallPrototypes = {	
 
 
