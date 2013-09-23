5d335f1d3d04e54c2a153efffb85035bedaafa22
Kenix157 <Kenix157@b2e8e5e1-9cd8-92f6-82b0-49c0d31916f8>
2013-09-23 16:50:06 +0000

    Added some functions: SetCameraTarget;GetCameraInterior;GetCameraMatrix;SetCameraInterior;SetCameraMatrix;

--- [gamemodes]/[amx]/pmta-wrapper/server/syscalls.lua
+++ [gamemodes]/[amx]/pmta-wrapper/server/syscalls.lua
@@ -162,7 +162,36 @@ function SetCameraTarget( amx, player, target )
 	return setCameraTarget( player, target );
 end
 
+function GetCameraInterior( amx, player )
+	return getCameraInterior( player );
+end
 
+function GetCameraMatrix( amx, player, refCameraX, refCameraY, refCameraZ, refTargetX, refTargetY, refTargetZ, refRoll, refFov )
+	local cameraX, cameraY, cameraZ, targetX, targetY, targetZ, roll, fov = getCameraMatrix( player );
+	writeMemFloat(amx, refCameraX, cameraX)
+	writeMemFloat(amx, refCameraY, cameraY)
+	writeMemFloat(amx, refCameraZ, cameraZ)
+	writeMemFloat(amx, refTargetX, targetX)
+	writeMemFloat(amx, refTargetY, targetY)
+	writeMemFloat(amx, refTargetZ, targetZ)
+	writeMemFloat(amx, refRoll, roll)
+	writeMemFloat(amx, refFov, fov)
+end
+
+--[[
+function GetCameraTarget( amx, player )
+	local playerID = getElemID( player )
+	
+	return playerID
+end]]
+
+function SetCameraInterior( amx, player, interior )
+	return setCameraInterior( player, interior );
+end
+
+function SetCameraMatrix( amx, player, positionX, positionY, positionZ )
+	return setCameraMatrix( player, positionX, positionY, positionZ );
+end
 
 function acos(amx, f)
 	return float2cell(math.acos(f))
@@ -292,6 +321,11 @@ g_SAMPSyscallPrototypes = {
 	SpawnPlayer	= { 'p', 'f', 'f', 'f' };
 	FadeCamera = { 'p', 'b' };
 	SetCameraTarget = { 'p', 'p' };
+	GetCameraInterior = { 'p' };
+	GetCameraMatrix = { 'p', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r' };
+	--GetCameraTarget = { 'p' };
+	SetCameraInterior = { 'p', 'i' };
+	SetCameraMatrix = { 'p', 'f', 'f', 'f' };
 
 	acos = {'f'},
 	asin = {'f'},
