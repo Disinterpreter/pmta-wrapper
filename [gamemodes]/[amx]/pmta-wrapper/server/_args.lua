d99a98fd83dbf2c654c00380e5c3087f1e857d7a
ru.mysqld <ru.mysqld@b2e8e5e1-9cd8-92f6-82b0-49c0d31916f8>
2013-09-24 10:57:36 +0000

    preparation for splitting the prototypes

---
+++ [gamemodes]/[amx]/pmta-wrapper/server/_args.lua
@@ -0,0 +1,128 @@
+function argsToMTA(amx, prototype, ...)
+	if type(amx) == 'userdata' then
+		local amxName = table.find(g_LoadedAMXs, 'cptr', amx)
+		if not amxName then
+			print('argsToMTA: No amx found for provided cptr')
+			return 0
+		end
+		amx = g_LoadedAMXs[amxName]
+	end
+	
+	local args = { ... }
+	local val
+	local argMissing = false
+	local colorArgs
+	for i,val in ipairs(args) do
+		vartype = prototype[i]
+		if vartype == 'b' then			-- boolean
+			val = val ~= 0
+		elseif vartype == 'c' then		-- color
+			if not colorArgs then
+				colorArgs = {}
+			end
+			colorArgs[i] = { binshr(val, 24) % 0x100, binshr(val, 16) % 0x100, binshr(val, 8) % 0x100 }		-- r, g, b
+			val = val % 0x100			-- a
+		elseif vartype == 'p' then		-- player
+			val = g_Players[val] and g_Players[val].elem
+		elseif vartype == 'v' then		-- vehicle
+			val = g_Vehicles[val] and g_Vehicles[val].elem
+		elseif vartype == 'e' then
+			val = g_Peds[val] and g_Peds[val].elem
+		end
+		if val == nil then
+			val = false
+			argMissing = true
+		end
+		args[i] = val
+	end
+	if colorArgs then
+		local indexOffset = 0
+		for i,colorArg in pairs(colorArgs) do
+			for j,color in ipairs(colorArg) do
+				table.insert(args, i+j-1 + indexOffset, color)
+			end
+			indexOffset = indexOffset + 3
+		end
+	end
+	
+	return args, argMissing
+end
+local argsToMTA = argsToMTA
+
+function argsToSAMP(amx, prototype, ...)
+	if type(amx) == 'userdata' then
+		local amxName = table.find(g_LoadedAMXs, 'cptr', amx)
+		if not amxName then
+			print('argsToSAMP: No amx found for provided cptr')
+			return 0
+		end
+		amx = g_LoadedAMXs[amxName]
+	end
+	
+	local args = { ... }
+	for i,v in ipairs(args) do
+		if type(v) == 'nil' then
+			args[i] = 0
+		elseif type(v) == 'boolean' then
+			args[i] = v and 1 or 0
+		elseif type(v) == 'string' then
+			-- keep unmodified
+		elseif type(v) == 'number' then
+			if prototype[i] == 'f' then
+				args[i] = float2cell(v)
+			end
+		elseif type(v) == 'userdata' then
+			args[i] = isElement(v) and getElemID(v)
+		else
+			args[i] = 0
+		end
+	end
+	return args
+end
+
+function syscall(amx, svc, prototype, ...)		-- svc = service number (= index in native functions table) or name of native function
+	if type(amx) == 'userdata' then
+		local amxName = table.find(g_LoadedAMXs, 'cptr', amx)
+		if not amxName then
+			return 0
+		end
+		amx = g_LoadedAMXs[amxName]
+	end
+	local fnName = type(svc) == 'number' and amx.natives[svc] or svc
+	local fn = prototype.fn or _G[fnName]
+	if not fn and not prototype.client then
+		outputDebugString('syscall: function ' .. tostring(fn) .. 'doesn\'t exist', 1)
+		return
+	end
+
+	local args, argMissing = argsToMTA(amx, prototype, ...)
+	
+	if argMissing then
+		return 0
+	end
+	--[[
+	local logstr = fnName .. '('
+	for i,argval in ipairs(args) do
+		if i > 1 then
+			logstr = logstr .. ', '
+		end
+		logstr = logstr .. tostring(argval)
+	end
+	logstr = logstr .. ')'
+	print(logstr)
+	outputConsole(logstr)
+	--]]
+
+	local result
+	if prototype.client then
+		local player = table.remove(args, 1)
+		clientCall(player, fnName, amx.name, unpack(args))
+	else
+		result = fn(amx, unpack(args))
+		if type(result) == 'boolean' then
+			result = result and 1 or 0
+		end
+	end
+	--print('syscall returned ' .. tostring(result or 0))
+	return result or 0
+end
